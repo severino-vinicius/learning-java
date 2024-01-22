e9b9b199f8ea7f6ad295a239d5efefeb3a34f8ec
Severino Vinicius <severino.sales95@gmail.com>
2024-01-22 15:46:00 -0300

    [Elective Java] - w02 d01 - Abstration

---
+++ learningJava/src/main/java/abstraction/electronic/Application.java
@@ -0,0 +1,12 @@
+package abstraction.electronic;
+
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Iniciando sistema...");
+
+    Television television = new Television();
+
+    System.out.println("Finalizando sistema...");
+  }
+}
---
+++ learningJava/src/main/java/abstraction/electronic/Television.java
@@ -0,0 +1,8 @@
+package abstraction.electronic;
+
+public class Television {
+
+  public Television() {
+    System.out.println("Construtor da classe Television sendo executado...");
+  }
+}
