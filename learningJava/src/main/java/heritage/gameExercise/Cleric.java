d9d3aa5268ead5387a7abe8d7a2fd9479e856d13
Severino Vinicius <severino.sales95@gmail.com>
2024-01-23 16:59:37 -0300

    [Elective Java] - w02 d02 - This Super Overload

--- learningJava/src/main/java/heritage/gameExercise/Cleric.java
+++ learningJava/src/main/java/heritage/gameExercise/Cleric.java
@@ -12,6 +12,18 @@ public class Cleric extends PlayableCharacter {
     System.out.println("The Cleric casts a spell");
   }
 
+  public void heal() {
+    System.out.println("The cleric is healing with a basic ability.");
+  }
+
+  public void heal(String potion) {
+    System.out.printf("The cleric is healing with the potion %s. %n", potion);
+  }
+
+  public void heal(String spell, int power) {
+    System.out.printf("The Cleric is using the spell %s with a power of %d. %n", spell, power);
+  }
+
   public void setMagic(String magic) {
     this.magic = magic;
   }
--- learningJava/src/main/java/heritage/gameExercise/CreateCharacter.java
+++ learningJava/src/main/java/heritage/gameExercise/CreateCharacter.java
@@ -4,22 +4,30 @@ public class CreateCharacter {
 
   public static void main(String[] args) {
 
-    Warrior warrior = new Warrior();
-    warrior.setName("Aragorn");
-    warrior.setRace("Human");
-    warrior.setWeapon("Sword");
-
-    System.out.printf("%s is of the %s race and uses a %s%n", warrior.getName(), warrior.getRace(), warrior.getWeapon());
-
     Cleric cleric = new Cleric();
-    cleric.setName("Gandalf");
-    cleric.setRace("Maiar");
-    cleric.setMagic("Light");
+    cleric.heal();
+    cleric.heal("Heal");
+    cleric.heal("Light", 7);
 
-    warrior.move();
-    warrior.attack();
-
-    cleric.move();
-    cleric.attack();
+//    Warrior warrior = new Warrior();
+//    warrior.setName("Aragorn");
+//    warrior.setRace("Human");
+//    warrior.setWeapon("Sword");
+//
+//    System.out.printf("%s is of the %s race and uses a %s%n", warrior.getName(), warrior.getRace(), warrior.getWeapon());
+//
+//    Cleric cleric = new Cleric();
+//    cleric.setName("Gandalf");
+//    cleric.setRace("Maiar");
+//    cleric.setMagic("Light");
+//
+//    warrior.move();
+//    warrior.attack();
+//
+//    cleric.move();
+//    cleric.attack();
+//
+//    Barbarian barbarian = new Barbarian();
+//    barbarian.move();
   }
 }
