b8272bcb22e79a5e2dffabb3b778db8a61c22574
Severino Vinicius <severino.sales95@gmail.com>
2024-01-31 21:33:12 -0300

    [Eletiva Java - W02D03] - Creating a Podcast Rest API

---
+++ podcast/src/main/java/com/dev/podcast/controller/dto/PodcastCreationDto.java
@@ -0,0 +1,10 @@
+package com.dev.podcast.controller.dto;
+
+import com.dev.podcast.model.Podcast;
+
+public record PodcastCreationDto(String name, String url) {
+
+//  public Podcast toEntity() {
+//    Podcast podcast = new Podcast();
+//  }
+}
